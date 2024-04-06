85b5bff450b432c3bb3e76e36f1fd2ff6f0cfe24
Dhaval <110185908+Dhaval404ronin@users.noreply.github.com>
2024-04-06 12:28:47 +0530

    Update background.js

--- Flappy bird/scripts/background.js
+++ Flappy bird/scripts/background.js
@@ -1,15 +1,20 @@
+// Define the Background class
 class Background {
     constructor() {
+        // Source position and size of the background image
         this.sX = 0;
         this.sY = 0;
         this.w = 275;
         this.h = 226;
+        // Initial position of the background
         this.x = 0;
         this.y = canvas.height - 226;
     };
 
+    // Method to draw the background on the canvas
     draw() {
-        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this. y, this.w, this.h);
-        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.w, this. y, this.w, this.h);
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        // Draw a second copy of the background for seamless looping
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.w, this.y, this.w, this.h);
     }
 }
